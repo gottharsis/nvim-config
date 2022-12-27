006bcc779e9d51d61df6d51ed8a38f538838edb7
Ayush Nene <ayush3108@gmail.com>
2022-12-27 12:23:00 -0500

    clean up

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,38 +4,65 @@ local configs = require'lspconfig/configs'
 local wk = require('which-key')
 local M = {}
 
-wk.register({
-    ['gD'] = {'<cmd>lua vim.lsp.buf.declaration()<CR>', "Go to Declaration"},
-    ['gd'] = {'<cmd>lua vim.lsp.buf.definition()<CR>', "Go to definition"},
-    K = {'<cmd>lua vim.lsp.buf.hover()<cr>', 'LSP Hover'},
+-- wk.register({
+--     ['gD'] = {'<cmd>lua vim.lsp.buf.declaration()<CR>', "Go to Declaration"},
+--     ['gd'] = {'<cmd>lua vim.lsp.buf.definition()<CR>', "Go to definition"},
+--     K = {'<cmd>lua vim.lsp.buf.hover()<cr>', 'LSP Hover'},
 
-    ['gi']= { '<cmd>lua vim.lsp.buf.implementation()<CR>' , "Go to implementation"},
-    ['<C-k>']= { '<cmd>lua vim.lsp.buf.signature_help()<CR>' , "Method signature"},
-    ['<leader>wa']= { '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' , "Add workspace folder"},
-    ['<leader>wr']= { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' , "Remove workspace folder"},
-    ['<leader>wl']= { '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>' , "List workspace folders"},
-    ['<leader>D']= { '<cmd>lua vim.lsp.buf.type_definition()<CR>' , "Type Definition"},
-    ['<leader>rn']= { '<cmd>lua vim.lsp.buf.rename()<CR>' , "LSP Rename"},
-    ['gr']= { '<cmd>lua vim.lsp.buf.references()<CR>' , "Show References"},
-    ['<leader>ca']= { '<cmd>lua vim.lsp.buf.code_action()<CR>' , "Code action"},
-    -- ['<leader>ca']= { '<cmd>lua vim.lsp.buf.range_code_action()<CR>' , ""},
-    -- ['<leader>e']= { '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>' , "Line Diagnostics"},
-    -- ['[d']= { '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>' , "Previous Diagnostic"},
-    -- [']d']= { '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>' , "Next Diagnostic"},
-    ['<leader>q']= { '<cmd>lua vim.diagnostic.setloclist()<CR>' , "Set Diagnostic loclist"},
-    ['<leader>so']= { [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]] , "Document Symbols"},
-    [ '<leader>F' ]= { [[<cmd>lua vim.lsp.buf.formatting()<CR>]] , "LSP Format"},
-}, 
-    {
-        mode = "n",
-        buffer = bufnr
-    })
+--     ['gi']= { '<cmd>lua vim.lsp.buf.implementation()<CR>' , "Go to implementation"},
+--     ['<C-k>']= { '<cmd>lua vim.lsp.buf.signature_help()<CR>' , "Method signature"},
+--     ['<leader>wa']= { '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' , "Add workspace folder"},
+--     ['<leader>wr']= { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' , "Remove workspace folder"},
+--     ['<leader>wl']= { '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>' , "List workspace folders"},
+--     ['<leader>D']= { '<cmd>lua vim.lsp.buf.type_definition()<CR>' , "Type Definition"},
+--     ['<leader>rn']= { '<cmd>lua vim.lsp.buf.rename()<CR>' , "LSP Rename"},
+--     ['gr']= { '<cmd>lua vim.lsp.buf.references()<CR>' , "Show References"},
+--     ['<leader>ca']= { '<cmd>lua vim.lsp.buf.code_action()<CR>' , "Code action"},
+--     -- ['<leader>ca']= { '<cmd>lua vim.lsp.buf.range_code_action()<CR>' , ""},
+--     -- ['<leader>e']= { '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>' , "Line Diagnostics"},
+--     -- ['[d']= { '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>' , "Previous Diagnostic"},
+--     -- [']d']= { '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>' , "Next Diagnostic"},
+--     ['<leader>q']= { '<cmd>lua vim.diagnostic.setloclist()<CR>' , "Set Diagnostic loclist"},
+--     ['<leader>so']= { [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]] , "Document Symbols"},
+--     [ '<leader>F' ]= { [[<cmd>lua vim.lsp.buf.formatting()<CR>]] , "LSP Format"},
+-- }, 
+--     {
+--         mode = "n",
+--         buffer = bufnr
+--     })
 
 
 local on_attach = function(_, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     local opts = { noremap = true, silent = true }
+
+    wk.register({
+        ['gD'] = {'<cmd>lua vim.lsp.buf.declaration()<CR>', "Go to Declaration"},
+        ['gd'] = {'<cmd>lua vim.lsp.buf.definition()<CR>', "Go to definition"},
+        K = {'<cmd>lua vim.lsp.buf.hover()<cr>', 'LSP Hover'},
+
+
+        ['gi']= { '<cmd>lua vim.lsp.buf.implementation()<CR>' , "Go to implementation"},
+        ['<C-k>']= { '<cmd>lua vim.lsp.buf.signature_help()<CR>' , "Method signature"},
+        ['<leader>wa']= { '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' , "Add workspace folder"},
+        ['<leader>wr']= { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' , "Remove workspace folder"},
+        ['<leader>wl']= { '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>' , "List workspace folders"},
+        ['<leader>D']= { '<cmd>lua vim.lsp.buf.type_definition()<CR>' , "Type Definition"},
+        ['<leader>rn']= { '<cmd>lua vim.lsp.buf.rename()<CR>' , "LSP Rename"},
+        ['gr']= { '<cmd>lua vim.lsp.buf.references()<CR>' , "Show References"},
+        ['<leader>ca']= { '<cmd>lua vim.lsp.buf.code_action()<CR>' , "Code action"},
+
+
+        ['<leader>q']= { '<cmd>lua vim.diagnostic.setloclist()<CR>' , "Set Diagnostic loclist"},
+        ['<leader>so']= { "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>" , "Document Symbols"},
+        [ '<leader>F' ]= { "<cmd>lua vim.lsp.buf.formatting()<CR>" , "LSP Format"},
+    },
+    {
+        mode = "n",
+        buffer = bufnr
+    })
+
 end
 M.on_attach = on_attach
 
@@ -50,7 +77,6 @@ local servers = {
 
 
 local mk_config = function(config) 
-    -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     config.on_attach = on_attach
     config.capabilities = capabilities
@@ -83,9 +109,6 @@ local sources = {
 
 
 -- trouble.nvim setup
-
-
-
 local function setup()
     null_ls.setup({ sources = sources })
 
@@ -106,31 +129,6 @@ local function setup()
         lspconfig[server].setup(mk_config(config))
     end
 
-    wk.register({
-        ['gD'] = {'<cmd>lua vim.lsp.buf.declaration()<CR>', "Go to Declaration"},
-        ['gd'] = {'<cmd>lua vim.lsp.buf.definition()<CR>', "Go to definition"},
-        K = {'<cmd>lua vim.lsp.buf.hover()<cr>', 'LSP Hover'},
-
-
-        ['gi']= { '<cmd>lua vim.lsp.buf.implementation()<CR>' , "Go to implementation"},
-        ['<C-k>']= { '<cmd>lua vim.lsp.buf.signature_help()<CR>' , "Method signature"},
-        ['<leader>wa']= { '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' , "Add workspace folder"},
-        ['<leader>wr']= { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' , "Remove workspace folder"},
-        ['<leader>wl']= { '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>' , "List workspace folders"},
-        ['<leader>D']= { '<cmd>lua vim.lsp.buf.type_definition()<CR>' , "Type Definition"},
-        ['<leader>rn']= { '<cmd>lua vim.lsp.buf.rename()<CR>' , "LSP Rename"},
-        ['gr']= { '<cmd>lua vim.lsp.buf.references()<CR>' , "Show References"},
-        ['<leader>ca']= { '<cmd>lua vim.lsp.buf.code_action()<CR>' , "Code action"},
-
-
-        ['<leader>q']= { '<cmd>lua vim.diagnostic.setloclist()<CR>' , "Set Diagnostic loclist"},
-        ['<leader>so']= { "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>" , "Document Symbols"},
-        [ '<leader>F' ]= { "<cmd>lua vim.lsp.buf.formatting()<CR>" , "LSP Format"},
-    },
-    {
-        mode = "n",
-        buffer = bufnr
-    })
 end
 M.setup = setup
 
--- lua/config/misc.lua
+++ lua/config/misc.lua
@@ -1,75 +1,21 @@
 -- configurations for smaller plugins, or general keybindings
 local wk = require('which-key')
 
-
 --------------
 -- File Tree
 --------------
 require('nvim-tree').setup{}
-
-vim.cmd 'nnoremap <C-n> :NvimTreeFindFileToggle<CR>'
-
-
-
-------------------------------------
--- Hop.nvim settings
-------------------------------------
--- local hop = require('hop')
--- hop.setup()
-
--- local hopMappings = {
---     ['<Leader><Leader>'] = {
---         name = "Hop motions",
---         w = { "<cmd>HopWordMW<cr>", "Word" },
---         b = { "<cmd>HopWordMW<cr>", "Word" },
---         j = { "<cmd>HopLinestartMW<cr>", "Lines" },
---         k = { "<cmd>HopLinestartMW<cr>", "Lines" },
---         f = { "<cmd>HopChar1MW<cr>", "Single Character" },
---         F = { "<cmd>HopChar1MW<cr>", "Single Character" },
---     }
--- }
-
--- wk.register(hopMappings, { mode = "n" })
--- wk.register(hopMappings, { mode = "v" })
--- wk.register(hopMappings, { mode = "o" })
--- vim.cmd [[
---     nnoremap <Leader><Leader>w :HopWord<cr>
---     nnoremap <Leader><Leader>j :HopLinestart<cr>
---     nnoremap <Leader><Leader>k :HopLinestart<cr>
--- ]]
---
--- require 'lightspeed'.setup {}
-
-
-------------------------------------
--- Autopairs setup
-------------------------------------
-
--- vim.cmd [[
--- au filetype tex     let b:AutoPairs = AutoPairsDefine({ 
---     \'$' : '$', '$$' : '$$', '\\{' : '\\}', 
---     \'\\left[': '\\right]', '\\left(' : '\\right)',
---     \'``': "''",
--- \}, ['"'])
--- ]]
-
---------------
--- Easy Align
---------------
-
 wk.register({
-    ['ga'] = {'<Plug>(EasyAlign)', "Easy Align"}
+    ['<C-n>'] = {"<cmd>NvimTreeFindFileToggle<cr>", "Toggle file tree"}
 })
-wk.register({
-    ['ga'] = {'<Plug>(EasyAlign)', "Easy Align"}
-}, { mode = 'x' })
+
+-- vim.cmd 'nnoremap <C-n> :NvimTreeFindFileToggle<CR>'
+
+
 
 ------------------------------------
 -- General Settings
 ------------------------------------
-wk.register {
-    -- ['<Leader>ee'] = {'<cmd>edit ~/.config/nvim/init.lua<cr>', "Edit Configuration"}
-}
 
 -- buffer navigation
 wk.register({
@@ -104,8 +50,3 @@ imap jk <esc>
 ]]
 
 vim.cmd [[ command! CDC cd %:p:h ]]
--- nmap <Leader>ee :edit ~/.config/nvim/init.lua<cr>
--------------
--- Processing
--------------
-vim.cmd [[ autocmd BufNewFile,BufRead *.pde set ft=java syntax=java ]]
