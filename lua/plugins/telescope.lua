98d181b75c8929b93ad00d762b71d303f62a44e1
Ayush Nene <ayush3108@gmail.com>
2023-04-05 13:38:32 -0400

    switch telescope layout to make it like helix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,16 +7,37 @@ return {
             build = "make"
         }
     },
-    config = function() 
-        require('telescope').setup()
+    config = function()
+        require('telescope').setup({
+            defaults = {
+                layout_strategy = "flex",
+                layout_config = { prompt_position = "top" },
+                sorting_strategy = "ascending"
+            }
+        })
         require('telescope').load_extension('fzf')
     end,
     keys = {
-        { "<leader>/", "<cmd>Telescope live_grep<cr>", desc="Find in Files (Grep)" },
-        { "<leader><space>", "<cmd>Telescope find_files<cr>", desc="Fuzzy Search File"},
-        { ",,", "<cmd>Telescope buffers<cr>", "List Buffers" },
-        { "<leader>e", function () require("telescope.builtin").find_files({ cwd = "~/.config/nvim" }) end, desc="Configure [E]ditor"},
+        {
+            "<leader>/",
+            "<cmd>Telescope live_grep<cr>",
+            desc =
+            "Find in Files (Grep)"
+        },
+        {
+            "<leader><space>",
+            "<cmd>Telescope find_files<cr>",
+            desc =
+            "Fuzzy Search File"
+        },
+        { ",,",         "<cmd>Telescope buffers<cr>",      "List Buffers" },
+        {
+            "<leader>e",
+            function() require("telescope.builtin").find_files({ cwd = "~/.config/nvim" }) end,
+            desc =
+            "Configure [E]ditor"
+        },
         { "<leader>gb", "<cmd>Telescope git_branches<cr>", "Switch Branch" },
-        { "<leader>K", "<cmd>Telescope colorscheme<cr>", "Change Colorscheme" }
+        { "<leader>K",  "<cmd>Telescope colorscheme<cr>",  "Change Colorscheme" }
     }
 }
