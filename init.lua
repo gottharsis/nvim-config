b5bbb3a50c732cd21baeb7e3857c9576e93f05f7
Ayush Nene <anene@bloomberg.net>
2024-07-30 10:53:04 -0400

    move lazy spec to init.vim

--- init.lua
+++ init.lua
@@ -13,6 +13,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(require("lazy_spec"))
+require("lazy").setup{
+    { import = "plugins" },
+    require("language-plugins")({"cpp"}),
+}
 
 require("config")
--- lua/lazy_spec.lua
+++
@@ -1,4 +0,0 @@
-return {
-    { import = "plugins" },
-    require("language-plugins")({"cpp"}),
-}
