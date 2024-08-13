03651f6a46f2ecd04d2e48b3f884b27a21994ef1
Ayush Nene <anene@bloomberg.net>
2024-08-13 12:18:34 -0400

    update keybinds

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -12,7 +12,8 @@ return {
             { "<leader><space>", function() require("fzf-lua").files() end,          desc = "File Explorer" },
             { "<leader>/",       function() require("fzf-lua").live_grep_glob() end, desc = "Grep" },
             { "<leader>*",       function() require("fzf-lua").grep_cword() end,     desc = "Grep word under cursor" },
-            { "<leader>*",       function() require("fzf-lua").grep_visual() end,    desc = "Grep selection",      mode = { "v" } },
+            { "<leader>*",       function() require("fzf-lua").grep_visual() end,    desc = "Grep selection",        mode = { "v" } },
+            { "gr",              function() require("fzf-lua").lsp_references() end, desc = "LSP References" }
         }
     }
 }
