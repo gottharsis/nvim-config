5d64e02e277881d59691194bda638edb00749a89
AYUSH NENE <ANENE@bloomberg.net>
2025-02-10 18:41:44 +0000

    Fix rainbow delimiters config

--- lua/ide_plugins/rainbow-delimiters.lua
+++ lua/ide_plugins/rainbow-delimiters.lua
@@ -2,6 +2,7 @@ return {
     {
         "HiPhish/rainbow-delimiters.nvim",
         config = function()
+            local rainbow_delimiters = require("rainbow-delimiters")
             vim.g.rainbow_delimiters = {
                 strategy = {
                     [''] = rainbow_delimiters.strategy['global'],
