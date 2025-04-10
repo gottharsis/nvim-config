44c514a7857d3068cc31b0327b6d1dd46860ef38
AYUSH NENE <ANENE@bloomberg.net>
2025-04-10 18:31:48 +0000

    add testing

---
+++ lua/ide_plugins/trouble.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        "folke/trouble.nvim",
+        opts = {
+            modes = {
+                neotest_errors = {
+                    mode = 'diagnostics',
+                    filter = { source = function(diag) 
+                        return string.match(diag.source, "neotest") ~= nil
+                    end }
+                }
+            }
+        }, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    }
+}
